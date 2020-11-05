643e71044b09b97a2e380085257e4ce6b233be60
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2020-11-06 00:37:02 +0800

    Need to fix the todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import { API } from "aws-amplify";
+import { List } from "antd";
+import "antd/dist/antd.css";
+import { listTodos } from "./graphql/queries";
+import "./App.css";
+
+interface IForm {
+  name: string;
+  description: string;
+}
+
+interface IInitialState {
+  notes: [];
+  loading: boolean;
+  error: boolean;
+  form: IForm;
+}
+
+interface IAction {
+  type: string;
+  payload: any;
+}
+
+const initialState: IInitialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: "", description: "" }
+};
+
+function reducer(state: IInitialState, action: IAction): IInitialState {
+  switch (action.type) {
+    case "SET_NOTES":
+      return { ...state, notes: action.payload, loading: false };
+    case "ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  async function fetchTodos() {
+    try {
+      const notesData = await API.graphql({
+        query: listTodos
+      });
+      dispatch({ type: "SET_NOTES", payload: notesData });
+    } catch (error) {}
+  }
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,43 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      clientID
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      clientID
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      clientID
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
